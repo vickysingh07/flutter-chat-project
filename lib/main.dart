695db4525da708ae3f5c71d67812c7f72472a53e
Vicky <vicky1910098@akgec.ac.in>
2020-10-04 00:55:50 +0530

    chapter 2C

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:let_us_chat/screens/login_screen.dart';
+import 'package:let_us_chat/screens/registration_screen.dart';
 import 'package:let_us_chat/screens/welcome_screen.dart';
-// import 'package:let_us_chat/screens/login_screen.dart';
-// import 'package:let_us_chat/screens/registration_screen.dart';
-// import 'package:let_us_chat/screens/chat_screen.dart';
+import 'package:let_us_chat/screens/chat_screen.dart';
 
 void main() => runApp(FlashChat());
 
@@ -15,7 +15,13 @@ class FlashChat extends StatelessWidget {
           bodyText1: TextStyle(color: Colors.black54),
         ),
       ),
-      home: WelcomeScreen(),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
+      },
     );
   }
 }
