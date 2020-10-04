7bb59968515ab63deaa76eb0ab755a31166e72de
Vicky <vicky1910098@akgec.ac.in>
2020-10-05 01:59:48 +0530

    chapter 8C

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'login_screen.dart';
 import 'registration_screen.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static String id = 'welcome_screen';
@@ -48,9 +49,9 @@ class _WelcomeScreenState extends State<WelcomeScreen>
                   child: Container(
                       child: Image.asset('images/logo.png'), height: 60.0),
                 ),
-                Text(
-                  'Flash Chat',
-                  style: TextStyle(
+                TypewriterAnimatedTextKit(
+                  text: ['Flash Chat'],
+                  textStyle: TextStyle(
                     fontSize: 45.0,
                     fontWeight: FontWeight.w900,
                   ),
